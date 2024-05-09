df60b2c60846634dfd97addf759542156189edb5
Igor <mapurungaigor@gmail.com>
2024-05-09 00:05:57 -0300

    Criado o inicio do Crud

---
+++ Crud/Cliente.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crud
+{
+    internal class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Cpf { get; set; }
+
+
+        public void cadastrarCliente(List<Cliente> ClientesCadastrados, Cliente cliente)
+        {
+            Console.Clear();
+            List<Cliente> clientes = new List<Cliente>();
+
+            Console.WriteLine("Digite o nome do cliente que deseja cadastrar: ");
+            string nomeDoCliente = Console.ReadLine();
+            cliente.Nome = nomeDoCliente;
+            Console.WriteLine("Digite o Email do cliente ");
+            string emailDoCliente = Console.ReadLine();
+            cliente.Email = emailDoCliente;
+            Console.WriteLine("Digite o Cpf do Cliente: ");
+            string CpfDoCliente = Console.ReadLine();
+            cliente.Cpf = CpfDoCliente;
+            ClientesCadastrados.Add(cliente);
+            Console.WriteLine("CLIENTE CADASTRADO COM SUCESSO");
+            Thread.Sleep(4000);
+            Console.Clear();
+    
+            
+
+        }
+
+        public void exibirClienteCadastrado(List<Cliente> ClientesCadastrados)
+        {
+            foreach (Cliente clienteCadastrado in ClientesCadastrados)
+            {
+                Console.WriteLine("Lista de Clientes Cadastrados:");
+                Console.WriteLine($"Nome: {clienteCadastrado.Nome}");
+                Console.WriteLine($"Email: {clienteCadastrado.Email}");
+                Console.WriteLine($"CPF: {clienteCadastrado.Cpf}");
+                Console.WriteLine("----------------------");
+            }
+        }
+
+        public void atualizarNomeDoCliente(List<Cliente> ClientesCadastrados)
+        {
+            Console.WriteLine("Digite o nome do cliente que deseja atualizar: ");
+            string verificarCliente = Console.ReadLine();
+            Cliente clienteEncontrado = ClientesCadastrados.FirstOrDefault(c => c.Nome == verificarCliente);
+
+            if (clienteEncontrado != null)
+            {
+                Console.WriteLine("Qual o nome deve ser inserido ?");
+                string novoNome = Console.ReadLine();
+                clienteEncontrado.Nome = novoNome;
+            }
+            else
+            {
+                Console.WriteLine("Cliente não encontrado.");
+            }
+        }
+    }
+}
---
+++ Crud/Program.cs
@@ -0,0 +1,53 @@
+﻿using Crud;
+
+Cliente cliente = new Cliente();
+List<Cliente> ClientesCadastrados = new List<Cliente>();
+
+
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("BEM VINDO AO MEU SISTEMA DE CADASTRO DE CLIENTES");
+    Console.WriteLine();
+    Console.WriteLine("DIGITE 1 PARA CADASTRAR UM NOVO CLIENTE");
+    Console.WriteLine();
+    Console.WriteLine("DIGITE 2 PARA EXIBIR UM CLIENTE");
+    Console.WriteLine();
+    Console.WriteLine("DIGITE 3 PARA ATUALIZAR UM CLIENTE");
+    Console.WriteLine();
+    Console.WriteLine("DIGITE 4 PARA DELETAR UM CLIENTE");
+    Console.WriteLine();
+    Console.WriteLine("DIGITE 5 PARA SAIR DO PROGRAMA");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    switch (opcaoEscolhidaNumerica)
+    {
+        case 1:
+            cliente.cadastrarCliente(ClientesCadastrados, cliente);
+            break;
+        case 2:
+            cliente.exibirClienteCadastrado(ClientesCadastrados);
+            break;
+        case 3:
+            cliente.atualizarNomeDoCliente(ClientesCadastrados);
+            break;
+        case 4:
+           // cliente.deletarCliente(ClientesCadastrados);
+            break;
+        case 5:
+            Console.WriteLine("Tchau tchau :)");
+            break;
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+}
+
+ExibirOpcoesDoMenu();
+   
+
+
+
+
